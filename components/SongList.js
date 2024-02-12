c4be4322dc8f47c0032ee9e978d608c41763639e
kurushiba <kurushiba55555@gmail.com>
2024-02-12 13:04:21 +0900

    modify

--- components/SongList.js
+++ components/SongList.js
@@ -8,21 +8,23 @@ export function SongList(props) {
 
   return (
     <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
-      {new Array(20).map(() => {
-        return (
-          <div className="flex-none cursor-pointer ">
-            <img
-              alt="thumbnail"
-              src={
-                'https://i.scdn.co/image/ab67616d0000b2738b7a8c1322028d45a8355f7a'
-              }
-              className="mb-2 rounded"
-            />
-            <h3 className="text-lg font-semibold">Song Name</h3>
-            <p className="text-gray-400">By Artist</p>
-          </div>
-        );
-      })}
+      {Array(20)
+        .fill()
+        .map(() => {
+          return (
+            <div className="flex-none cursor-pointer ">
+              <img
+                alt="thumbnail"
+                src={
+                  'https://i.scdn.co/image/ab67616d0000b2738b7a8c1322028d45a8355f7a'
+                }
+                className="mb-2 rounded"
+              />
+              <h3 className="text-lg font-semibold">Song Name</h3>
+              <p className="text-gray-400">By Artist</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
